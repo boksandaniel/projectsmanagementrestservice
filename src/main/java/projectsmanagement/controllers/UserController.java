acc3b058551317893d0a434e35f5834504b57706
Daniel <boksandaniel@yahoo.com>
2019-06-08 16:16:59 +0300

    news

--- src/main/java/projectsmanagement/controllers/UserController.java
+++ src/main/java/projectsmanagement/controllers/UserController.java
@@ -3,6 +3,7 @@ package projectsmanagement.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.SortDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -23,7 +24,7 @@ public class UserController
     private UserRepository userRepository;
 
     @GetMapping("/users")
-    public String userList(Model model, @RequestParam(defaultValue = "") String name)
+    public String userList(Model model, @SortDefault("name") @RequestParam(defaultValue = "") String name)
     {
         model.addAttribute("users", userService.findByName(name));
         return "views/userList";
